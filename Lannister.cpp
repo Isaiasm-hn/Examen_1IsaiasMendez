e5ea9aa48ccba41f4c89fc36515a57644e753392
isaiasm-hn <isaiasm_hn@unitec.edu>
2017-02-17 16:00:27 -0600

    Agregue el cpp de Lannister

---
+++ Lannister.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include "Lannister.h"
+using namespace std;
+
+void Lannister::setNombre(string x){
+   boss=x;
+}
+
+string Lannister::getNombre(){
+   return boss;
+}
+
+void Lannister::setEmblema(string x){
+   emblema=x;
+}
+
+string Lannister::getEmblema(){
+   return emblema;
+}
+
+void Lannister::setLema(string x){
+   lema=x;
+}
+
+string Lannister::getLema(){
+   return lema;
+}
+
+void Lannister::setDinero (int x){
+		dinero=x;
+
+}
+
+void Lannister::getDinero (){
+	return=dinero;
+}
+
+void Lannister::setFuerza (int x){
+		fuerza=x;
+
+}
+
+void Lannister::getFuerza (){
+	return=fuerza;
+}
+
+void Lannister::setIntegrantes (int x){
+		integrantes=x;
+
+}
+
+void Lannister::getIntegrantes (){
+	return=integrantes;
+}
+
