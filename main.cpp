cf3ce7d3d605bd5890bfe879cade49d692a1473e
isaiasm-hn <isaiasm_hn@unitec.edu>
2017-02-17 18:21:33 -0600

    Creando el main!

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "Targaryen.h"
+#include "Starks.h"
+#include "Dothraki.h"
+#include "f_nobles.h"
+#include "Guardia.h"
+#include "Lannister.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+	Lannister ln;
+	Targaryen tr;
+	Starks st;
+	vector <Dothraki> doth;
+	vector <Guardia> guard;
+	vectoy <f_nobles> nobles;
+	int opc;
+	do{
+		switch (){
+
+		}
+	}while()
+}
+
